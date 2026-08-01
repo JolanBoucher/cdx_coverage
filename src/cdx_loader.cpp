0565347f30fcdff8569cb5c4489faf1fec065993
Jolan Boucher <jolan.boucher.1@ulaval.ca>
2026-07-31 20:51:03 -0400

    started implementation of `cdx_loader` module

---
+++ src/cdx_loader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jolan on 2026-07-31.
+//
+
+#include "cdx_loader.h"
---
+++ src/cdx_loader.h
@@ -0,0 +1,39 @@
+/**
+ * @file cdx_loader.h
+ * @brief High-level CDX loading API.
+ *
+ * Provides:
+ *  - component loading
+ *  - query initialization
+ *  - graph-wide loading
+ *  - component inspection
+ */
+
+#ifndef CDX_COVERAGE_CDX_LOADER_H
+#define CDX_COVERAGE_CDX_LOADER_H
+
+#include "cdx_types.h"
+#include <filesystem>
+#include <optional>
+
+namespace cdx {
+
+    [[nodiscard]]QueryData loadQuery(
+        const std::filesystem::path& cdx_path,
+        std::size_t component_id,
+        std::optional<std::pair<PosBp,PosBp>> query_range = std::nullopt,
+        bool circular = false
+    );
+
+    [[nodiscard]]GlobalData loadGlobal(
+        const std::filesystem::path& cdx_path
+    );
+
+    void inspectComponent(
+        const std::filesystem::path& cdx_path,
+        std::optional<std::size_t> component_id = std::nullopt
+    );
+
+}
+
+#endif //CDX_COVERAGE_CDX_LOADER_H
